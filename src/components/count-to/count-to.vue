7f8f610e98c23720805f298ddc24808846739590
jiesen <jieqqq678@163.com>
2019-09-12 06:46:48 +0800

    1

---
+++ src/components/count-to/count-to.vue
@@ -0,0 +1,114 @@
+<template>
+	<div>
+		<slot></slot><span :class="countClass"
+					:id="eleId"></span>
+	</div>
+</template>
+
+<script>
+import CountUp from 'countup'
+
+export default {
+  name: 'CountTo',
+  computed: {
+    eleId () {
+      return `count_up_${this._uid}`
+    },
+    countClass () {
+      return [
+        'count-to-number',
+        this.className
+      ]
+    }
+
+  },
+  data () {
+    return {
+      counter: {}
+    }
+  },
+  props: {
+    /**
+																										 *  @description 初始值
+																										*/
+    startVal: {
+      type: Number,
+      default: 0
+    },
+    /**
+							 * @description 最终值
+							 */
+    endVal: {
+      type: Number,
+      required: true
+    },
+    /**
+																										* @description 小数点后保留几位小数
+																										*/
+    decimals: {
+      type: Number,
+      default: 0
+    }, /**
+								 * @description 动画开始延迟时间
+								 */
+    delay: {
+      type: Number,
+      default: 0
+    },
+    /**
+																										* @description 渐变时长
+																										*/
+    duration: {
+      type: Number,
+      default: 1
+    },
+    /**
+																								* @description 是否使用变速效果
+																								*/
+    useEasing: {
+      type: Boolean,
+      default: false
+    },
+    /**
+																								* @description 是否使用分组
+																								*/
+    useGrouping: {
+      type: Boolean,
+      default: true
+    },
+    /**
+																								* @description 分隔符
+																								*/
+    separator: {
+      type: String,
+      default: ','
+    }, /**
+     * @description 整数和小数分割符号
+     */
+    decimal: {
+      type: String,
+      default: '.'
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.counter = new CountUp(this.eleId, this.startVal, this.endVal, this.decimals, this.duration, {
+        useEasing: this.useEasing,
+        useGrouping: this.useGrouping,
+        separator: this.separator,
+        decimal: this.decimal
+      })
+      setTimeout(() => {
+        this.counter.start()
+      }, this.delay)
+    })
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/counttos.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<count-to :end-val="endVal">
+			<span>总金额: </span>
+		</count-to>
+	</div>
+</template>
+
+<script>
+import CountTo from '@/components/count-to'
+
+export default {
+  name: 'count_to',
+  components: {
+    CountTo
+  },
+  data () {
+    return {
+      endVal: 100
+    }
+  }
+}
+</script>
+
+<style>
+</style>
